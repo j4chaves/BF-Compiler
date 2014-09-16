a5f7bd94b56463d64ab04e24bfda57d911e0bd64
j4chaves <jacobjc3@gmail.com>
2014-09-16 00:23:27 -0400

    Fleshed out doPArse.  Hopefully I did it right.

--- BF Compiler/src/BF.java
+++ BF Compiler/src/BF.java
@@ -12,14 +12,6 @@ public class BF
 		void accept (Visitor v);
 	}
 	
-	public static void main(String args[])
-	{
-		// Parsing string to tree
-		Node hello = parse("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
-		// Code generation from tree
-		hello.accept(new CompilerVisitorJava());
-	}
-	
 	public static abstract class Sequence implements Node 
 	{
 		//Declare linked list of child nodes
@@ -27,67 +19,67 @@ public class BF
 		//This allows to add child node to linked list
 		public void add(Node instruction)
 		{
-			
+			list.add(instruction);
 		}
 	}
 	
 	/*
 	 * Program and Loop are interior nodes. All others are leaf nodes
 	 */
-	public static class Loop extends Sequence 
+	public static class Loop extends Sequence  implements Node
 	{
 		public void accept (Visitor v)
 		{
-			
+			v.visit(this);
 		}
 	}
-	public static class Program extends Sequence
+	public static class Program extends Sequence implements Node
 	{
 		public void accept (Visitor v)
 		{
-			
+			v.visit(this);
 		}
 	}
-	public static class Left
+	public static class Left implements Node
 	{
 		public void accept (Visitor v)
 		{
-			
+			v.visit(this);
 		}
 	}
-	public static class Right
+	public static class Right implements Node
 	{
 		public void accept (Visitor v)
 		{
-			
+			v.visit(this);
 		}
 	}
-	public static class Increment
+	public static class Increment implements Node
 	{
 		public void accept (Visitor v)
 		{
-			
+			v.visit(this);
 		}
 	}
-	public static class Decrement
+	public static class Decrement implements Node
 	{
 		public void accept (Visitor v)
 		{
-			
+			v.visit(this);
 		}
 	}
-	public static class Input
+	public static class Input implements Node
 	{
 		public void accept (Visitor v)
 		{
-			
+			v.visit(this);
 		}
 	}
-	public static class Output
+	public static class Output implements Node
 	{
 		public void accept (Visitor v)
 		{
-			
+			v.visit(this);
 		}
 	}
 	
@@ -98,13 +90,29 @@ public class BF
 	private static Sequence doParse (StringBuffer buf, Sequence seq)
 	{
 		//Store a single character
-		
+		char character = buf.charAt(0);
 		//As long as the string buffer isn't empty
-		while()
+		while(buf.length() > 0)
 		{
-			//consumer one character from the start of the buffer
-			
+			//consume one character from the start of the buffer
+			buf.deleteCharAt(0);
 			//Add proper node classes to seq, based on consumed character
+			if (character == '>')
+				seq.add(new Right());
+			else if (character == '<')
+				seq.add(new Left());
+			else if (character == '+')
+				seq.add(new Increment());
+			else if (character == '-')
+				seq.add(new Decrement());
+			else if (character == '.')
+				seq.add(new Output());
+			else if (character == ',')
+				seq.add(new Input());
+			else if (character == '[')
+				seq.add(new Loop());		//I don't think this is right.  I'm not sure how to handle these loop chars
+			else if (character == ']')		//Remember to come back and fix them cause they're probably wrong
+				seq.add(new Loop());
 		}
 		return seq;
 	}
@@ -135,15 +143,47 @@ public class BF
 		void visit(Program program);
 	}
 	
-	public class CompilerVisitorJava implements Visitor
+	public static class CompilerVisitorJava implements Visitor
 	{
 		public void visit(Left left)
+		{
+			
+		}
 		public void visit(Right right)
+		{
+			
+		}
 		public void visit(Increment increment)
+		{
+			
+		}
 		public void visit(Decrement derement)
+		{
+			
+		}
 		public void visit(Input input)
+		{
+			
+		}
 		public void visit(Output output)
+		{
+			
+		}
 		public void visit(Loop loop)
+		{
+			
+		}
 		public void visit(Program program)
+		{
+			
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		// Parsing string to tree
+		Node hello = parse("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
+		// Code generation from tree
+		hello.accept(new CompilerVisitorJava());
 	}
 }
